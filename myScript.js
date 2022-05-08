cbd31eb9fbde04b6bb5cfd071bde535ec538ed69
Jakub Kołodziej 28 <jakub.kolodziej28@microsoft.wsei.edu.pl>
2022-05-08 11:00:13 +0200

    v1

--- myScript.js
+++ myScript.js
@@ -23,14 +23,56 @@ function Reset_DX() {
 }
 
 
-var AX = document.getElementById("AX").value;
-var BX = document.getElementById("BX").value;
 
-//Instruction MOVE
+//Instruction MOVE//
+    ////First ROW/////
 function mov_ax_bx() {
     var AX = document.getElementById("AX").value;
     document.getElementById("BX").value = AX;
-    document.getElementById("AX").value = "";
-    //TO DO:
-    //Dokończyć inskrukcje MOV
-}
+    }
+function mov_bx_ax() {
+    var BX = document.getElementById("BX").value;
+    document.getElementById("AX").value=BX;
+    }
+    function mov_cx_ax() {
+        var CX = document.getElementById("CX").value;
+        document.getElementById("AX").value = CX;
+        }
+        function mov_dx_ax() {
+            var DX = document.getElementById("DX").value;
+            document.getElementById("AX").value = DX;
+            }
+            ////Second ROW/////
+            function mov_ax_cx() {
+                var AX = document.getElementById("AX").value;
+                document.getElementById("CX").value = AX;
+                }
+                function mov_bx_cx() {
+                    var BX = document.getElementById("BX").value;
+                    document.getElementById("CX").value = BX;
+                    }
+                    function mov_cx_bx() {
+                        var CX = document.getElementById("CX").value;
+                        document.getElementById("BX").value = CX;
+                        }
+                        function mov_dx_bx() {
+                            var DX = document.getElementById("DX").value;
+                            document.getElementById("BX").value = DX;
+                            }
+                             ////Third ROW/////
+                            function mov_ax_dx() {
+                                var AX = document.getElementById("AX").value;
+                                document.getElementById("DX").value = AX;
+                                }
+                                function mov_bx_dx() {
+                                    var BX = document.getElementById("BX").value;
+                                    document.getElementById("DX").value = BX;
+                                    }
+                                    function mov_cx_dx() {
+                                        var CX = document.getElementById("CX").value;
+                                        document.getElementById("DX").value = CX;
+                                        }
+                                        function mov_dx_cx() {
+                                            var DX = document.getElementById("DX").value;
+                                            document.getElementById("CX").value = DX;
+                                            }
