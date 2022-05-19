bd76648d8d578d5f9f3ad8372f01c8a0ba0fd1bf
Jakub Kołodziej <94216113+JakubKolodziej21@users.noreply.github.com>
2022-05-19 20:48:54 +0200

    V1.02

--- myScript.js
+++ myScript.js
@@ -1,3 +1,64 @@
+//Validation 
+window.addEventListener("DOMContentLoaded",()=>{
+    //AX//
+    const AX = document.getElementById("AX");
+    const inputPattern = /^[A-Fa-f0-9]*$/;
+    AX.addEventListener('input',()=>{
+            let validateInput = inputPattern.test(AX.value);
+        if(validateInput == false || AX.value.length == 5)
+                {
+                    AX.value = AX.value.substr(0,AX.value.length-1);
+                }
+    
+    })
+     //BX//
+     const BX = document.getElementById("BX");
+     
+     BX.addEventListener('input',()=>{
+              let validateInput = inputPattern.test(BX.value);
+         if(validateInput == false || BX.value.length == 5)
+                 {
+                     BX.value = BX.value.substr(0,BX.value.length-1);
+                 }
+     
+     })
+      //CX//
+      const CX = document.getElementById("CX");
+    
+    CX.addEventListener('input',()=>{
+            let validateInput = inputPattern.test(CX.value);
+        if(validateInput == false || CX.value.length == 5)
+                {
+                    CX.value = CX.value.substr(0,CX.value.length-1);
+                }
+    
+    })
+
+      //DX//
+      const DX = document.getElementById("DX");
+        DX.addEventListener('input',()=>{
+    
+        let validateInput = inputPattern.test(DX.value);
+        if(validateInput == false || DX.value.length == 5)
+                {
+                    DX.value = DX.value.substr(0,DX.value.length-1);
+                }
+    
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
 //Console Panel
 function Reset() {
     document.getElementById("console").reset();
